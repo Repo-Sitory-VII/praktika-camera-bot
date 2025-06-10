57a25707306c80c19adfe031a73a59e3afc4cc2b
Nikita Khrolenko <khronik8504@mail.ru>
2025-06-11 09:26:30 +1000

    Nothing really changed

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import telebot
 
 bot = telebot.TeleBot("7611552831:AAGtLC6JQI4ujjb5IZrg9sCGE-YwJUUVOtc", parse_mode=None)
-
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "РАБОТАЕТ")
 	
 bot.infinity_polling()
-print("F")
